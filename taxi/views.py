4232325a5a2526bd860c998f1ac4f60a6f6cfff4
Dede <specialized8393@gmail.com>
2024-05-02 17:53:34 +0100

    Created ManufacturerListView CarListView CarDetailView DriverListView DriverDetailView taxi/... manufacturers/ cars/ cars/pk/ drivers/ drivers/username

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all()
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer")
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+    queryset = Driver.objects.exclude(is_superuser=True)
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("car")
+
+    def get_object(self, queryset=None):
+        username = self.kwargs.get("username", None)
+        return get_object_or_404(Driver, username=username)
 
 
 def index(request):
